d1d99f68b5caa60fbcbbee77f79f266c3d045ac3
Rick South <rick@natamus.com>
2025-02-12 23:29:45 +0100

    (v4.9) Fixed the hotkey trigger not working correctly.

--- Forge/src/main/java/com/natamus/hiddenrecipebook/forge/events/ForgeKeyMappingRegister.java
+++ Forge/src/main/java/com/natamus/hiddenrecipebook/forge/events/ForgeKeyMappingRegister.java
@@ -6,12 +6,10 @@ import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber(value = Dist.CLIENT)
 public class ForgeKeyMappingRegister {
     @SubscribeEvent
-	public void registerKeyBinding(RegisterKeyMappingsEvent e) {
+	public static void registerKeyBinding(RegisterKeyMappingsEvent e) {
     	Variables.hotkey = new KeyMapping("hiddenrecipebook.key.togglebook", InputConstants.Type.KEYSYM, 258, "key.categories.misc");
     	e.register(Variables.hotkey);
     }

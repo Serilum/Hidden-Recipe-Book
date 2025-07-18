e0755578e578b394f6a174e0ef9a6ce0ec5e760a
Rick South <rick@natamus.com>
2025-07-18 02:58:18 +0200

    Hidden Recipe Book source code for 1.21.8.

---
+++ Forge/src/main/java/com/natamus/hiddenrecipebook/ModForge.java
@@ -0,0 +1,50 @@
+package com.natamus.hiddenrecipebook;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.hiddenrecipebook.forge.config.IntegrateForgeConfig;
+import com.natamus.hiddenrecipebook.forge.events.ForgeBookGUIEvent;
+import com.natamus.hiddenrecipebook.forge.events.ForgeKeyMappingRegister;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import com.natamus.hiddenrecipebook.util.Reference;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge(FMLJavaModLoadingContext modLoadingContext) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		if (!FMLEnvironment.dist.equals(Dist.CLIENT)) {
+			return;
+		}
+
+		BusGroup busGroup = modLoadingContext.getModBusGroup();
+
+		FMLLoadCompleteEvent.getBus(busGroup).addListener(this::loadComplete);
+		RegisterKeyMappingsEvent.getBus(busGroup).addListener(ForgeKeyMappingRegister::registerKeyBinding);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(modLoadingContext);
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		ForgeBookGUIEvent.registerEventsInBus();
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
